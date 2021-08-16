7810c8563d44534ee75a263533776dc2992b4874
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-08-16 12:08:29 +0430

    render all components in App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Home from './components/Home'
+import Sidebar from './components/Sidebar'
+import Modal from './components/Modal'
+
 function App() {
-  return <div className='App'></div>
+  return (
+    <div>
+      <Home />
+      <Sidebar />
+      <Modal />
+    </div>
+  )
 }
 
 export default App
