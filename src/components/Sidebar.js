60aa34b6f1da703d430db8d981ed306fae6ddb75
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-08-16 12:07:19 +0430

    add Sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { FaTimes } from 'react-icons/fa'
+import './sidebar.css'
+import { links, social } from '../data'
+
+function Sidebar() {
+  return (
+    <aside className='sidebar show-sidebar'>
+      <button className='close-sidebar'>
+        <FaTimes />
+      </button>
+      <div className='sidebar-header'>
+        <h3>My Sidebar</h3>
+      </div>
+
+      <ul className='links'>
+        {links.map((link) => {
+          const { id, url, icon, text } = link
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon} {text}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+
+      <ul className='social'>
+        {social.map((item) => {
+          const { id, url, icon } = item
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          )
+        })}
+      </ul>
+    </aside>
+  )
+}
+
+export default Sidebar
